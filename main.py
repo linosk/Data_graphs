feee744db41bac1b8c239dfbbced3067db067389
linosk <linakoskar@gmail.com>
2023-02-04 19:27:19 +0100

    Script finished

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 import os
 import shutil
-import re
 import zipfile
 
+#Path to zipped data
 raw_data_path = os.getcwd()
 raw_data_path = raw_data_path[:raw_data_path.rfind("/")] + "/Raw_data"
 
+#Path in which uzipped and sorted date will be placed
 ready_data_path = os.getcwd() + "/Files"
 
-print(raw_data_path)
-print(ready_data_path)
-
-
+#Delete "Files" directory if it exists
 if os.path.exists(ready_data_path):
     shutil.rmtree(ready_data_path)
 
+#Create "Files" directory if it does not exist
 if not os.path.exists(ready_data_path):
     os.mkdir(ready_data_path)
 
-target = raw_data_path + re.match("*.zip")
-target = 'drive-download-20221017T162533Z-001.zip'
+#Define target zip file name located in raw_data_path that will be copied nad unzipped to directory "/Files" placed in ready_data_path
+target_file = 'drive-download-20221017T162533Z-001.zip'
 
-"""
+#Copy target file to "Files" directory
+shutil.copy(raw_data_path+ "/" +target_file,ready_data_path)
 
-shutil.copy(path.replace('Files','')+target,path+'\\'+target)
+#Change directory to "Files"
+os.chdir(ready_data_path)
+current_path = os.getcwd()
 
-os.chdir(path)
-
-path = os.getcwd()
-
-handle = zipfile.ZipFile(target)
-
-handle.extractall(path)
-
+#Extract zipped file contents and then remove zipped file
+handle = zipfile.ZipFile(target_file)
+handle.extractall(current_path)
 handle.close()
+os.remove(target_file)
 
-os.remove(target)
-
-files = os.listdir(path)
-
+#Rename files names to be more readable
+files = os.listdir(current_path)
 for file in files:
     year=file[8:12]
     month=file[12:14]
@@ -46,55 +42,37 @@ for file in files:
     hour=file[16:18]
     minute=file[18:20]
     second=file[20:22]
-    new_name = day+'.'+month+'.'+year+'-'+hour+';'+minute+';'+second+'.csv'
+    new_name = day+'.'+month+'.'+year+'-'+hour+':'+minute+':'+second+'.csv'
     os.rename(file,new_name)
 
-lower_freq = path+"\\26GHz"
-higher_freq = path+"\\38GHz"
-noise = path+"\\Noise"
+#Get list of files's names
+files_renamed = os.listdir(current_path)
+files_renamed.sort()
 
+#Create directories used for sorting extracted files
+lower_freq = "26GHz"
+higher_freq = "38GHz"
+noise = "Noise"
 if os.path.exists(lower_freq):
     shutil.rmtree(lower_freq)
-
 if os.path.exists(higher_freq):
     shutil.rmtree(higher_freq)
-
 if os.path.exists(noise):
     shutil.rmtree(noise)
-
 if not os.path.exists(lower_freq):
     os.mkdir(lower_freq)
-
 if not os.path.exists(higher_freq):
     os.mkdir(higher_freq)
-
 if not os.path.exists(noise):
     os.mkdir(noise)
 
-
-files = os.listdir(path)
-
-files.remove('26GHz')
-files.remove('38GHz')
-files.remove('Noise')
-
+#Sort files using created directories
 i = 0
-
-buffer = ''
-
-lower_freq_phrase = ',#Center frequency [Hz]:,+2.60000000000E+010'
-
-for file in files:
-    text_file = open(file,'r')
-    buffer = text_file.read()
-    text_file.close()
-    if not buffer.find(lower_freq_phrase) == -1:
-        shutil.move(path+'\\'+file,path+'\\26GHz'+'\\'+file)
+for file in files_renamed:
+    if i<46:
+        shutil.move(current_path+'/'+file,current_path+'/26GHz'+'/'+file)
+    elif i>=46 and i<92:
+        shutil.move(current_path+'/'+file,current_path+'/38GHz'+'/'+file)
     else:
-        if i<92:
-            shutil.move(path+'\\'+file,path+'\\38GHz'+'\\'+file)
-        else:
-            shutil.move(path+'\\'+file,path+'\\Noise'+'\\'+file)
-    i=i+1
-
-    """
+        shutil.move(current_path+'/'+file,current_path+'/Noise'+'/'+file)
+    i = i + 1
