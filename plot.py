3bdecbdfb74d9566f18f2a90de0b4ef3db6a9c42
linosk <linakoskar@gmail.com>
2023-02-06 11:07:15 +0100

    Reading from the file succeded

---
+++ plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("Files/Noise/13.09.2022-12:52:35.csv", skiprows=28, encoding_errors='ignore')
+#df = pd.read_csv("Files/Noise/13.09.2022-12:52:35.csv")
+
+
+#python how to prepare data https://www.youtube.com/results?search_query=python+how+to+prepare+data
+#python how to pandas, mathploib(?) etc.
