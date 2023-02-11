3445122f62432e153b9012a15017928ea4dea9b0
linosk <linakoskar@gmail.com>
2023-02-11 16:34:33 +0100

    Function creating title for a graph added

--- functions.py
+++ functions.py
@@ -12,3 +12,26 @@ def calculate_path_loss(a,gtx,grx):
     path_loss = -a + find_lin_value(gtx) + find_lin_value(grx)
     path_loss = find_log_value(path_loss)
     return path_loss
+
+def find_title(scenario):
+    if(scenario[0]=='2' and scenario[1]=='6'):
+        band="26"
+    else:
+        band="38"
+
+    if(scenario[2]=='L'):
+        conditions="LOS"
+    else:
+        conditions="NLOS"
+
+    distance=float(scenario[3:5])
+    distance=distance/10
+    distance=str(distance)
+
+    if(scenario[5]=='T'):
+        axis="czasu."
+    else:
+        axis="częstotliwości."
+
+    title = "Wartość tłumienia propagacyjnego w paśmie "+band+"GHz"+", dla warunków "+conditions+", odległość miedzy antenami "+distance+" metrów, w zależności od "+axis
+    return title
