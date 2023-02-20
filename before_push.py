de68229f1862ae82a55824ded37db55c6f175e8c
linosk <linakoskar@gmail.com>
2023-02-20 16:50:04 +0100

    .jpg and .zip files are also deleted

--- before_push.py
+++ before_push.py
@@ -12,5 +12,5 @@ if os.path.exists(pycache):
 current_path=os.getcwd()
 content=os.listdir(current_path)
 for image in content:
-    if image.endswith(".png"):
+    if image.endswith(".png") or image.endswith(".jpg") or image.endswith(".zip"):
         os.remove(os.path.join(current_path,image))
