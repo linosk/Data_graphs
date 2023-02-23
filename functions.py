7fbfc139dcf8b2814db6945de6818120214f0b30
linosk <linakoskar@gmail.com>
2023-02-23 20:40:50 +0100

    Functions geneariting titles

--- functions.py
+++ functions.py
@@ -6,6 +6,20 @@ def find_lin_value(value):
 def find_log_value(value):
     return 10*log10(value)
 
+def find_lin_value_arr(value):
+    new_value = [0]*len(value)
+    for i in range(len(value)):
+        new_value[i] = pow(10,value[i]/10)
+
+    return new_value
+
+def find_log_value_arr(value):
+    new_value = [0]*len(value)
+    for i in range(len(value)):
+        new_value[i] = 10*log10(value[i])
+
+    return new_value
+
 #a is a measured value that has to be multiply by -1. Then the gain of antennas have to be maid liner, and the sum of all three values has to 
 #be turned into decibels.
 def calculate_path_loss(a,gtx,grx):
@@ -13,25 +27,65 @@ def calculate_path_loss(a,gtx,grx):
     path_loss = find_log_value(path_loss)
     return path_loss
 
-def find_title(scenario):
-    if(scenario[0]=='2' and scenario[1]=='6'):
-        band="26"
+def get_title_mean(scenario,pol,dom):
+    title = ""
+    title += scenario[0:2]+"-"
+    title += scenario[2]+"-"
+    title += scenario[3:5]+"-"
+    if pol == "V":
+        title += "V-"
     else:
-        band="38"
-
-    if(scenario[2]=='L'):
-        conditions="LOS"
+        title += "H-"
+    if dom == "F":
+        title += "F"
     else:
-        conditions="NLOS"
+        title += "T"
 
-    distance=float(scenario[3:5])
-    distance=distance/10
-    distance=str(distance)
+    return title
 
-    if(scenario[5]=='T'):
-        axis="czasu."
+def get_title_diff(scenario,dom):
+    title = ""
+    title += scenario[0:2]+"-"
+    title += scenario[2]+"-"
+    title += scenario[3:5]+"-"
+    if dom == "F":
+        title += "F-"
     else:
-        axis="częstotliwości."
+        title += "T-"
+    title += "Diff"
 
-    title = "Wartość tłumienia propagacyjnego w paśmie "+band+"GHz"+", dla warunków "+conditions+", odległość miedzy antenami "+distance+" metrów, w zależności od "+axis
+    return title
+
+def get_title_mean_var(scenario,pol,dom):
+    title = ""
+    title += scenario[0:2]+"-"
+    title += scenario[2]+"-"
+    title += scenario[3:5]+"-"
+    if pol == "V":
+        title += "V-"
+    else:
+        title += "H-"
+    if dom == "F":
+        title += "F-"
+    else:
+        title += "T-"
+    title += "Var"
+
+    return title
+
+def get_title_mean_std(scenario,pol,dom):
+    title = ""
+    title += scenario[0:2]+"-"
+    title += scenario[2]+"-"
+    title += scenario[3:5]+"-"
+    if pol == "V":
+        title += "V-"
+    else:
+        title += "H-"
+    if dom == "F":
+        title += "F-"
+    else:
+        title += "T-"
+    title += "Std"
+
     return title
