cae722e1923e812ac1a61cc42625cf1e0b54fe3e
linosk <linakoskar@gmail.com>
2023-03-11 22:16:59 +0100

    Function make_plot works, extend functionality

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 from math import log10
+import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
+import shutil
 
 def find_lin_value(value):
     return pow(10,value/10)
@@ -90,3 +93,67 @@ def get_title_mean_std(scenario,pol,dom):
         title += "T"
 
     return title
+
+#TYPE FCT
+# Frequency 2 or 6 - 26GHz or 38GHz
+# Conditions L or N - LOS or NLOS
+# Type A, S, X, V, H, DD
+# A - average path loss, 
+# S - standard deviation of path loss, 
+# X - XPD values, 
+# V - receiver antenna has vertical polarization, 
+# H - reciever antenna has horizontal polarization
+# DDD - distance in meters
+def make_plot(y_axis_one, y_axis_two, maxy, miny, x_axis, type):
+    width = 12
+    height = 5
+    path = '/home/me/Uni/Master/Graphs/Data_graphs'
+
+    plt.figure(figsize=(width,height))
+    if type[2] == 'A' or type[2] == 'S':
+        plt.plot(x_axis,y_axis_one, color='g',label='V-H')
+        plt.plot(x_axis,y_axis_two, color='r',label='V-V')
+    plt.legend(loc='upper left')
+    #MAKE 2f MODIFABLE
+    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.2f}'))
+    if type[0] == '2':
+        freg = '26'
+    else:
+        freg = '38'
+    if type[1] == 'L':
+        con = 'LOS'
+    else:
+        con = 'NLOS'
+    if type[2] == 'A':
+        title_part = '\u03B1'
+        file_part = 'AVG'
+    plt.title(f'Wartość średnia {title_part} w zależności od d, {freg}GHz, {con}')
+    if type[2] ==  'A' or type[2] == 'S':
+        plt.xlabel('d [m]')
+    if type[2] == 'A':
+        plt.ylabel(f'{title_part} [dB]')
+    plt.ylim(ymax=maxy, ymin=miny)
+    file_name = f'{freg}{con}{file_part}.jpg'
+    plt.savefig(file_name)
+    plt.close()
+    shutil.move(file_name,path+"/"+file_name)
+
+    #if type == 'AVG':
+    #    pass
+    #elif type == 'STD':
+    #    pass
+    #else:
+    #    pass
+
+    #plt.figure(figsize=(width,height))
+    #plt.plot(distance_LOS,average_LOS_H, color='g', label='V-H')
+    #plt.plot(distance_LOS,average_LOS_V, color='r', label='V-V')
+    #plt.legend(loc='upper left')
+    #plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.4f}'))
+    #plt.title("Wartość średnia tłumienia propagacyjnego w zależności od odległości, 26GHz, LOS")
+    #plt.xlabel("Odległość [m]")
+    #plt.ylabel("Wartość średnia tłumienia propagacyjengo [dB]")
+    #plt.ylim(ymax = maxy, ymin = miny)
+    #plt.savefig("26LOS"+".jpg")
+    #plt.close()
+    #shutil.move("26LOS.jpg","/home/me/Uni/Master/Graphs/Data_graphs/26LOS.jpg")
