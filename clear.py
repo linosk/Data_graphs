978c853c90f2e9d79e874cf5c5e0f5f73b5e4273
linosk <linakoskar@gmail.com>
2023-03-12 16:53:40 +0100

    Name change from before_push.py to clear.py

---
+++ clear.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+folders=["Files","__pycache__","Plots"]
+for folder in folders:
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+
+current_path=os.getcwd()
+content=os.listdir(current_path)
+for file in content:
+    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".zip") or file.endswith(".txt"):
+        os.remove(os.path.join(current_path,file))
