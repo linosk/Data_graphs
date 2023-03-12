e154dfeb9c136fa6debcde499616524c39991f9f
linosk <linakoskar@gmail.com>
2023-03-12 16:54:22 +0100

    Function to make plots complited

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from math import log10
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
+import os
 import shutil
 
 def find_lin_value(value):
@@ -94,66 +95,89 @@ def get_title_mean_std(scenario,pol,dom):
 
     return title
 
-#TYPE FCT
-# Frequency 2 or 6 - 26GHz or 38GHz
-# Conditions L or N - LOS or NLOS
-# Type A, S, X, V, H, DD
-# A - average path loss, 
-# S - standard deviation of path loss, 
-# X - XPD values, 
-# V - receiver antenna has vertical polarization, 
-# H - reciever antenna has horizontal polarization
-# DDD - distance in meters
+#TYPE FCVWDD
+# Frequency: 2 or 3 - 26GHz or 38GHz
+# Conditions: L or N - LOS or NLOS
+# Values: A or S - average path loss or standard deviation of path loss
+# What: P, X, V, H - what the plot will present, values for two polarization simultaneiusly, XPD values, average path loss for vertical polarization in the receiver antenna or average path loss for horizontal polarization in the receiver antenna
+# Distance: if needed specify the distance between antennas in meters otherwise pass DD
+
 def make_plot(y_axis_one, y_axis_two, maxy, miny, x_axis, type):
     width = 12
     height = 5
-    path = '/home/me/Uni/Master/Graphs/Data_graphs'
+    path = os.getcwd()
+    index = path.find('/Files')
+    path = path[:index]
+    path = path + '/Plots'
 
-    plt.figure(figsize=(width,height))
-    if type[2] == 'A' or type[2] == 'S':
-        plt.plot(x_axis,y_axis_one, color='g',label='V-H')
-        plt.plot(x_axis,y_axis_two, color='r',label='V-V')
-    plt.legend(loc='upper left')
-    #MAKE 2f MODIFABLE
-    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.2f}'))
     if type[0] == '2':
         freg = '26'
     else:
         freg = '38'
+
     if type[1] == 'L':
         con = 'LOS'
     else:
         con = 'NLOS'
+
     if type[2] == 'A':
-        title_part = '\u03B1'
-        file_part = 'AVG'
-    plt.title(f'Wartość średnia {title_part} w zależności od d, {freg}GHz, {con}')
-    if type[2] ==  'A' or type[2] == 'S':
+        if type[3] == 'P':
+            yaxis_part = '\u03B1'
+            title_part = f'{yaxis_part} od d dla obu polaryzacji, '
+            file_part = 'AVG_BOTH'
+        elif type[3] == 'X':
+            yaxis_part = 'XPD'
+            title_part = f'{yaxis_part} od d, '
+            file_part = 'AVG_XPD'
+        elif type[3] == 'V':
+            distance = int(type[4]) + int(type[5])/10
+            yaxis_part = '\u03B1'
+            title_part = f'{yaxis_part} polaryzacja V, odległość {distance} m '
+            file_part = f'AVG_VER_{type[4:]}'
+        elif type[3] == 'H':
+            distance = int(type[4]) + int(type[5])/10
+            yaxis_part = '\u03B1'
+            title_part = f'{yaxis_part} polaryzacja H, odległość {distance} m '
+            file_part = f'AVG_HOR_{type[4:]}'
+
+    else:
+        if type[3] == 'P':
+            yaxis_part = '\u03C3'
+            title_part = f'{yaxis_part} od d dla obu polaryzacji, '
+            file_part = 'STD_BOTH'
+        elif type[3] == 'X':
+            yaxis_part = 'XPD'
+            title_part = f'{yaxis_part} od d, '
+            file_part = 'STD_XPD'
+        elif type[3] == 'V':
+            distance = int(type[4]) + int(type[5])/10
+            yaxis_part = '\u03C3'
+            title_part = f'{yaxis_part} polaryzacja V, odległość {distance} m '
+            file_part = f'STD_VER_{type[4:]}'
+        elif type[3] == 'H':
+            distance = int(type[4]) + int(type[5])/10
+            yaxis_part = '\u03C3'
+            title_part = f'{yaxis_part} polaryzacja H, odległość {distance} m '
+            file_part = f'STD_HOR_{type[4:]}'
+
+    plt.figure(figsize=(width,height))
+
+    if type[3] == 'P':
+        plt.plot(x_axis,y_axis_one, color='g',label='V-H')
+        plt.plot(x_axis,y_axis_two, color='r',label='V-V')
+        plt.legend(loc='upper left')
+    else:
+        plt.plot(x_axis,y_axis_one)
+    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.2f}'))
+    plt.title(f'{title_part}{freg}GHz, {con}')
+    if type[3] == 'P' or type[3] == 'X':
         plt.xlabel('d [m]')
-    if type[2] == 'A':
-        plt.ylabel(f'{title_part} [dB]')
+    else:
+        plt.xlabel('f [GHz]')
+    plt.ylabel(f'{yaxis_part} [dB]')
+
     plt.ylim(ymax=maxy, ymin=miny)
-    file_name = f'{freg}{con}{file_part}.jpg'
+    file_name = f'{freg}_{con}_{file_part}.jpg'
     plt.savefig(file_name)
     plt.close()
     shutil.move(file_name,path+"/"+file_name)
-
-    #if type == 'AVG':
-    #    pass
-    #elif type == 'STD':
-    #    pass
-    #else:
-    #    pass
-
-    #plt.figure(figsize=(width,height))
-    #plt.plot(distance_LOS,average_LOS_H, color='g', label='V-H')
-    #plt.plot(distance_LOS,average_LOS_V, color='r', label='V-V')
-    #plt.legend(loc='upper left')
-    #plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.4f}'))
-    #plt.title("Wartość średnia tłumienia propagacyjnego w zależności od odległości, 26GHz, LOS")
-    #plt.xlabel("Odległość [m]")
-    #plt.ylabel("Wartość średnia tłumienia propagacyjengo [dB]")
-    #plt.ylim(ymax = maxy, ymin = miny)
-    #plt.savefig("26LOS"+".jpg")
-    #plt.close()
-    #shutil.move("26LOS.jpg","/home/me/Uni/Master/Graphs/Data_graphs/26LOS.jpg")
