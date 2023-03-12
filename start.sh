648e92293e9fe04558ef5f8e1494934dc19847ef
linosk <linakoskar@gmail.com>
2023-03-12 16:58:50 +0100

    Different scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
+python3 clear.py
 python3 sort_files.py
-python3 freq_sort.py
 python3 new_plot.py
-#python3 place_and_zip2.py
-
-#Change plot.py and place_and_zip.py in order to calculate Lprep in relation to distance
-#python3 plot.py
-#python3 place_and_zip.py
